12c13b00782b654bcfd370fd4ec2434559493fd6
Holden Davis <59069546+holden-davis-uca@users.noreply.github.com>
2022-11-02 06:23:59 -0500

    Moving to Java project instead of Python

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors/Client.java
@@ -0,0 +1,11 @@
+package RockPaperScissors;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("CLIENT");
+
+	}
+
+}
---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors/Manager.java
@@ -0,0 +1,11 @@
+package RockPaperScissors;
+
+public class Manager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("MANAGER");
+
+	}
+
+}
